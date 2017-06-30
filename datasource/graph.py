153d9fe1a4b3b82d613149d5887fb6b7e50198d3
musiclicn <musiclicn@gmail.com>
2017-06-29 23:54:06 -0400

    change bar width and picture width according to 30 min

--- datasource/graph.py
+++ datasource/graph.py
@@ -28,18 +28,20 @@ def draw_graph(ticker, df, lines):
     dec = df.trend == -1
     w = 12*60*60*1000 # half day in ms
 
+    width_30_min = 30*60*1000 # half day in ms
+
     df['open'] = df['high']
     df['close'] = df['low']
 
     TOOLS = "pan,wheel_zoom,box_zoom,reset,save"
 
-    p = figure(x_axis_type="datetime", tools=TOOLS, plot_width=1000, title=ticker)
+    p = figure(x_axis_type="datetime", tools=TOOLS, plot_width=1600, title=ticker)
     p.xaxis.major_label_orientation = pi/4
     p.grid.grid_line_alpha = 0.3
 
     p.segment(df.date, df.high, df.date, df.low, color="black")
-    p.vbar(df.date[inc], w, df.open[inc], df.close[inc], fill_color="#D5E1DD", line_color="black")
-    p.vbar(df.date[dec], w, df.open[dec], df.close[dec], fill_color="#F2583E", line_color="red")
+    p.vbar(df.date[inc], width_30_min, df.open[inc], df.close[inc], fill_color="#D5E1DD", line_color="black")
+    p.vbar(df.date[dec], width_30_min, df.open[dec], df.close[dec], fill_color="#F2583E", line_color="red")
 
     # p.line(df.date[inc], df.open[inc], line_width=3)
 
