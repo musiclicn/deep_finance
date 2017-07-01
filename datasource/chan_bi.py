1d26356b0f6a21087307ccb38d38d72b62f81121
musiclicn <musiclicn@gmail.com>
2017-07-01 17:09:34 -0400

    add dynamic BiGenerator and fix 3 bar bi bug

--- datasource/chan_bi.py
+++ datasource/chan_bi.py
@@ -13,20 +13,6 @@ class BiFactory(object):
     candidate_bi = []
 
 
-class WeakBi(object):
-    def __init__(self, bars):
-        # assert len(bars) > 0
-        self.bars = bars
-        self.trend = bars[0].trend
-
-    @property
-    def strength(self):
-        strength = 0
-        for bar in self.bars:
-            strength += bar.gravity_log
-        return strength
-
-
 class Bi(object):
     @staticmethod
     def create_bi(bars):
@@ -61,6 +47,20 @@ class Bi(object):
         start = (self.bars[0].time, self.bars[0].gravity)
         end = (self.bars[-1].time, self.bars[-1].gravity)
         return start, end
+
+
+class WeakBi(Bi):
+    def __init__(self, bars):
+        # assert len(bars) > 0
+        self.bars = bars
+        self.trend = bars[0].trend
+
+    @property
+    def strength(self):
+        strength = 0
+        for bar in self.bars:
+            strength += bar.gravity_log
+        return strength
 
 
 class PhantomBi(Bi):
@@ -102,6 +102,8 @@ def find_trend_reversed_bi(bar_queue, pre_bi_trend):
         else:
             assert len(bars_temp) >= 3
             return Bi(bars_temp)
+    if len(bars_temp) >= 3:
+        return Bi(bars_temp)
 
 
 def find_4_out_of_5_reversed_bi(bar_queue, pre_bi_trend):
@@ -149,6 +151,7 @@ def find_reversal_bi_and_weak_bi_pairs(bar_queue, pre_bi, weak_bi_pairs):
 
 
 # this needs improvement
+# this is not used currently
 def merge_weak_bi_pair(pre_bi, reversed_bi, weak_bi_pair):
     pair_strength = calculate_weak_bi_strength(weak_bi_pair)
     if pair_strength > 0:
@@ -211,3 +214,57 @@ def generate_bi(bars):
         trend_confirmed_bi.append(trend_reversed_bi)
 
     return ended_bi, trend_confirmed_bi
+
+
+class BiGenerator(object):
+
+    def __init__(self):
+        self.reverse_candidate_bars = deque()
+        self.weak_bi_pairs = []
+        self.trend_confirmed_bi = []
+        self.ended_bi = []
+        phantom_bi = PhantomBi(-1)
+        self.trend_confirmed_bi.append(phantom_bi)
+
+    @property
+    def cur_bi_trend(self):
+        return self.trend_confirmed_bi[-1].trend
+
+    def append_bar(self, bar):
+        """
+        if there is a trend confirmed ( this is always true because of phantom bi trick)
+            if bar is reverse trend, append to reverse_candidate_bars,
+                if reverse_candidate_bars become a reversed_trend_confirmed bi
+                    combine weak_bi into the trend_confirmed_bi
+                    move trend_confirmed_bi to ended_bi
+
+            else # bar is same trend
+                if reverse_candidate_bars is not empty
+                    create weak bi from reverse_candidate_bars
+                    append bar into reverse_candidate_bars
+        """
+        if len(self.trend_confirmed_bi) == 0:
+            raise IOError('there is no trend confirmed bi')
+
+        if bar.trend != self.cur_bi_trend:
+            self.reverse_candidate_bars.append(bar)
+            success, reverse_bi = try_find_trend_reversed_bi(self.reverse_candidate_bars, self.cur_bi_trend)
+            if success:
+                pre_bi = self.trend_confirmed_bi.pop()
+                for weak_bi_pair in self.weak_bi_pairs:
+                    pre_bi.merge_weak_bi_pair(weak_bi_pair)
+                self.ended_bi.append(pre_bi)
+                self.trend_confirmed_bi.append(reverse_bi)
+
+        else:
+            if len(self.weak_bi_pairs) == 0 and len(self.reverse_candidate_bars) == 0:
+                self.trend_confirmed_bi[-1].append_bar(bar)
+            elif len(self.reverse_candidate_bars) > 0:
+                self.reverse_candidate_bars.append(bar)
+                weak_bi_pair = find_weak_bi_pair(self.reverse_candidate_bars)
+                self.weak_bi_pairs.append(weak_bi_pair)
+            elif len(self.weak_bi_pairs) > 0 and len(self.reverse_candidate_bars) == 0:
+                bi1, bi2 = self.weak_bi_pairs[-1]
+                bi2.append_bar(bar)
+            else:
+                raise NotImplemented('unexpected condition')
